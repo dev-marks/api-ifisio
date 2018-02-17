61ca769364cd12732878f9a0daf7c1407a774389
Rony <ronysilvati@live.com>
2018-02-17 11:24:51 -0200

    Commit Inicial

---
+++ controllers/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class ApiController extends Controller
+{
+    public function actionIndex()
+    {
+        die("OK");
+    }
+}
